fbe657445c6b611010e57fa526a5f4f9ad1dba7f
vyas0320 <vyas0320@gmail.com>
2021-10-10 10:34:50 -0700

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -2,7 +2,6 @@
 function makeBigger() {
     document.getElementById("ftext").style.fontSize = "24pt";
 
-
 }
 
 function makeBold() {
@@ -10,6 +9,17 @@ function makeBold() {
     if (document.getElementById("bold").checked) {
         textarea.style.fontWeight = "bold";
         textarea.style.textDecoration = "none";
-        textarea.style.color = "green";
+        textarea.style.color = "blue";
+    } else {
+        textarea.style.fontWeight = "normal";
+        textarea.style.textDecoration = "none";
+        textarea.style.color = "black";
+
     }
+}
+
+function makeNormal() {
+    var textarea = document.getElementById("ftext");
+    document.getElementById("ftext").style.fontSize = "12pt";
+    
 }
